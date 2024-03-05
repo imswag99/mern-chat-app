8e636d58b4083bcca343d9b706e5cf1fd22966e5
Swagat Talukdar <swagattalukdar007@gmail.com>
2024-03-05 17:23:39 +0530

    update package.json

--- api/index.js
+++ api/index.js
@@ -23,7 +23,7 @@ app.use('/uploads', express.static(__dirname + '/uploads'));
 app.use(
   cors({
     credentials: true,
-    origin: "http://localhost:5173",
+    origin: "https://mern-chat-app-eight-beige.vercel.app/",
   })
 );
 app.use(cookieParser());
